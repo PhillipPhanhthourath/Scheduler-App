1ba560329c4da5d65161e4de8ec8b58ff9877c5f
PhillipPhanhthourath <phanhthourath.phillip@gmail.com>
2024-01-30 19:33:35 -0500

    added weekly view page and event adder button

--- app/src/main/java/com/example/scheduler_app/CalendarViewHolder.java
+++ app/src/main/java/com/example/scheduler_app/CalendarViewHolder.java
@@ -9,10 +9,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+    public final View parentView;
     public final TextView dayOfMonth;
     private final CalendarAdapter.OnItemListener onItemListener;
     public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener){
         super(itemView);
+        parentView = itemView.findViewById(R.id.parentView);
         dayOfMonth = itemView.findViewById(R.id.Cell_Day_Text);
         this.onItemListener = onItemListener;
         itemView.setOnClickListener(this);
--- app/src/main/java/com/example/scheduler_app/WeekViewActivity.java
+++ app/src/main/java/com/example/scheduler_app/WeekViewActivity.java
@@ -1,14 +1,64 @@
 package com.example.scheduler_app;
 
+import static com.example.scheduler_app.CalendarUtils.daysInMonthArray;
+import static com.example.scheduler_app.CalendarUtils.daysInWeekArray;
+import static com.example.scheduler_app.CalendarUtils.monthYearFromDate;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class WeekViewActivity extends AppCompatActivity {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class WeekViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
+
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week_view);
+        initWidget();
+        setWeekView();
+    }
+
+    private void initWidget() {
+        calendarRecyclerView = findViewById(R.id.Calendar_Recycler_View);
+        monthYearText = findViewById(R.id.Month_Year_Display);
+    }
+
+    private void setWeekView() {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> days = daysInWeekArray(CalendarUtils.selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(days, this);
+        RecyclerView.LayoutManager layoutmanager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutmanager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    public void previousWeekAction(View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks(1);
+        setWeekView();
+    }
+
+    public void nextWeekAction(View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks(1);
+        setWeekView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        String message = "Select Date " + dayText + " " + monthYearFromDate(CalendarUtils.selectedDate);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void newEventAction(View view) {
     }
 }
